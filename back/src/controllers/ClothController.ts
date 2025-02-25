3ff02f64e461abc50223d4565fcce05bde512164
Socrates Freitas <socratesfreitas0@gmail.com>
2025-02-25 11:30:10 -0300

    feat: ClothController implementado

--- back/src/controllers/ClothController.ts
+++ back/src/controllers/ClothController.ts
@@ -1,37 +1,147 @@
-import { PrismaClient } from "@prisma/client";
-import { warnEnvConflicts } from "@prisma/client/runtime/library";
-import { json, Request, Response } from "express";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
 
 const prisma = new PrismaClient();
 
 export enum ClothCategory {
-	BLUSA = "BLUSA",
-	CALCA = "CALÇA"
+  BLUSA = "BLUSA",
+  CALCA = "CALÇA",
+  CALCADO = "CALÇADO",
 }
 
-
-
 export class ClothController {
-	static async createCloth(request: Request, response: Response) {
-		try {
-			const { clothName, userId, clothType, clothFabric, clothColor, imgageUrl: imageUrl } =
-				request.body;
+  static async createCloth(request: Request, response: Response) {
+    try {
+      const { clothName, userId, clothCategory, clothType, clothColor } =
+        request.body;
+
+      const createClothInput: Prisma.ClothCreateInput = {
+        clothName: clothName,
+        user: {
+          connect: {
+            id: Number(userId),
+          },
+        },
+        clothCategory: clothCategory,
+        clothType: Number(clothType),
+        clothColor: clothColor,
+      };
+
+      const createdCloth = await prisma.cloth.create({
+        data: createClothInput,
+      });
+
+      response.status(201).json(createdCloth);
+    } catch (error: any) {
+      response.status(500).json({ message: error.message });
+    }
+  }
+
+  static async getClothes(request: Request, response: Response) {
+    try {
+      const { userId } = request.body;
+
+      const clothes = await prisma.cloth.findMany({
+        where: {
+          userId: typeof userId === "string" ? Number(userId) : undefined,
+        },
+      });
+
+      response.status(200).json(clothes);
+    } catch (error: any) {
+      response.status(500).json({ message: error.message });
+    }
+  }
+
+  static async getCloth(request: Request, response: Response) {
+    try {
+      const { userId, clothName } = request.body;
+
+      const cloth = await prisma.cloth.findUnique({
+        where: {
+          userId: Number(userId),
+          clothName: clothName,
+        },
+      });
+
+      if (!cloth) {
+        response.status(404).json({ message: "Cloth Not Fount" });
+        return;
+      }
+
+      response.status(200).json(cloth);
+    } catch (error: any) {
+      response.status(500).json({ message: error.message });
+    }
+  }
+
+  static async updateCloth(request: Request, response: Response) {
+    try {
+      const { clothName, userId, clothCategory, clothType, clothColor } =
+        request.body;
+
+      const updateClothInput: Prisma.ClothUpdateInput = {
+        clothName: clothName,
+        clothCategory: clothCategory,
+        clothType: Number(clothType),
+        clothColor: clothColor,
+      };
+
+      const clothToBeUpdated = await prisma.cloth.findUnique({
+        where: {
+          userId: Number(userId),
+          clothName: clothName,
+        },
+      });
+      if (!clothToBeUpdated) {
+        response.status(404).json({ message: "Cloth Not Found" });
+        return;
+      }
+
+      const updatedCLoth = await prisma.cloth.update({
+        data: updateClothInput,
+        where: {
+          userId: Number(userId),
+          clothName: clothName,
+        },
+      });
+
+      response.status(201).json(updatedCLoth);
+    } catch (error: any) {
+      response.status(500).json({ message: error.message });
+    }
+  }
+
+  static async deleteCloth(request: Request, response: Response) {
+    try {
+      const { userId, clothName } = request.body;
+
+      const deletedCloth = await prisma.cloth.delete({
+        where: {
+          clothName: clothName,
+        },
+      });
+      response.status(200).json(deletedCloth);
+    } catch (error: any) {
+      response.status(500).json({ message: error.message });
+    }
+  }
+
+  static async deleteClothes(request: Request, response: Response) {
+    try {
+      const { userId } = request.body;
+      const deletedCloth = await prisma.cloth.deleteMany({
+        where: {
+          userId: typeof userId === "string" ? Number(userId) : undefined,
+        },
+      });
+      response.status(200).json(deletedCloth);
+    } catch (error: any) {
+      response.status(500).json({ message: error.message });
+    }
+  }
 
 
-			const createdCloth = await prisma.cloth.create({
-				data: {
 
-					clothName: clothName,
-					clothColor: Number(clothColor),
-					clothType: Number(clothType),
-					clothFabric: Number(clothFabric),
-					imageUrl: imageUrl
 
-				}
-			}
-
-		} catch (error: any) {
-			response.status(201).json({ message: error.message });
-		}
-	}
 }
